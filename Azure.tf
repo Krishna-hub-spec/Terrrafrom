6c4140d5b2e1a703f6c792d1c6f1b409a5e60123
Krishna-hub-spec <krishnateja0982@gmail.com>
2024-10-16 13:16:28 -0700

    Update Azure.tf

--- Azure.tf
+++ Azure.tf
@@ -1,33 +1,40 @@
  provider "azurerm" {
   features {}
+
+  # (Optional) Specify the subscription ID to ensure you are working with the correct subscription
+  subscription_id = "36629079-fbde-4dce-b2b9-e42435dfa1d3"  # Replace this with your actual Azure subscription ID
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "example-resources"
-  location = "East US"
-}
-
+# Reference the existing resource group
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                  = "example-vm"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  size                  = "Standard_DS1_v2"
+  name                  = "Harness_IACM"   # Customize the VM name
+  location              = "(US) West Central US"      # Location should match the region of the existing resource group
+  resource_group_name   = "QuadrantFabricRG"  # Replace with your existing resource group name
+  size                  = "Standard_D2s_v3"  # VM size
 
   admin_username = "azureuser"
+
+  # SSH public key for VM access
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file("C:/Users/Hello/.ssh/id_rsa.pub")  # Provide the full path to your public key
+    public_key = file("C:/Users/Hello/.ssh/id_rsa.pub")  # Path to your SSH public key file
   }
 
+  # Define the OS disk configuration
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
 
+  # Define the image for the VM (Ubuntu 18.04 LTS)
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "18.04-LTS"
     version   = "latest"
+  }
+
+  tags = {
+    environment = "production"
   }
 }
